07ed47bdcfa032edd0838c6d94126cde6289ea0b
RaghavGarg <raghavgarg8219@gmail.com>
2025-09-24 22:02:45 +0530

    fix: CORS setup

--- frontend/src/services/Api.jsx
+++ frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://ngo-portal-8.onrender.com/api", withCredentials:true // backend URL
 });
 
 // Attach token to requests (if logged in)
