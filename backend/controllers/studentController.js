7ffd655c29a81ecc38038c1cdb282655d519da41
Deepak Singh Rawat <deepaksinghrawat141@gmail.com>
2025-09-24 22:11:31 +0530

    Update studentController.js

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -71,26 +71,7 @@ export const applyStudent = async (req, res) => {
       documents.cv = await uploadToCloudinary(files.cv[0].buffer, "cv");
     }
 
-    const documents = {
-      aadhaar: files.aadhaar
-        ? await uploadToCloudinary(files.aadhaar[0].buffer, "aadhaar")
-        : "",
-      reportCard: files.reportCard
-        ? await uploadToCloudinary(files.reportCard[0].buffer, "reportCard")
-        : "",
-      marksheet: files.marksheet
-        ? await uploadToCloudinary(files.marksheet[0].buffer, "marksheet")
-        : "",
-      granthiProof: files.granthiProof
-        ? await uploadToCloudinary(files.granthiProof[0].buffer, "granthiProof")
-        : "",
-      parentAadhaar: files.parentAadhaar
-        ? await uploadToCloudinary(files.parentAadhaar[0].buffer, "parentAadhaar")
-        : "",
-      cv: files.cv
-        ? await uploadToCloudinary(files.cv[0].buffer, "cv")
-        : "",
-    };
+    
 
 
     const student = await Student.create({
